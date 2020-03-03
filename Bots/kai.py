b83ed58af6e7cfdef803c8dd424f62a2ebef82d4
yawny666 <admin@devlabsf.com>
2020-03-03 23:57:17 +0000

    initial commit - broken code!

--- Bots/kai.py
+++ Bots/kai.py
@@ -1,5 +1,5 @@
-kai module for Live Oak's MegaBratBot project."""
+"""kai module for Live Oak's MegaBratBot project."""
 
 phrases = ("May the force be with you", "Never!","sup peeps","nice one!")
 
-greeting = "hi homeys"
+greeting = "hi homeys"qwertyuiop
